162d7bc9c0cab78691d3e04c10a6f9fa6814be3a
jovan996 <grgurjovan.jg@gmail.com>
2019-04-18 02:50:53 +0200

    agentski front update

--- AgentskiModulFront/src/app/app-routing.module.ts
+++ AgentskiModulFront/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './main/login/login.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [LoginComponent],
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [RouterModule, LoginComponent]
 })
 export class AppRoutingModule { }
