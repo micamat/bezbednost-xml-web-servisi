32d9e778d3cfd9deaf3a12e18d580263b2e1d417
micamat <matijevicmilica96@gmail.com>
2019-05-16 00:57:21 +0200

    mitigacija ranjivosti

--- Backend/App/src/main/java/project/AppApplication.java
+++ Backend/App/src/main/java/project/AppApplication.java
@@ -1,7 +1,12 @@
 package project;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
@@ -10,4 +15,15 @@ public class AppApplication {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Bean
+	public ServletContextInitializer initializer() {
+	    return new ServletContextInitializer() {
+			
+	        @Override
+	        public void onStartup(ServletContext servletContext) throws ServletException {
+	            servletContext.setInitParameter("springJspExpressionSupport", "false");
+	        }
+	    };
+	}
+	
 }
