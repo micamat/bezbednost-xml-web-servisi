600f0ba3e72df7f92ba3b306381a19b27e284148
DejanPredojevic <dejanpredojevic11@gmail.com>
2019-05-22 14:32:32 +0200

    Agentski modul start

---
+++ AgentskiModul/src/main/java/agentski/modul/app/AgentskiModulApplication.java
@@ -0,0 +1,13 @@
+package agentski.modul.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentskiModulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentskiModulApplication.class, args);
+	}
+
+}
