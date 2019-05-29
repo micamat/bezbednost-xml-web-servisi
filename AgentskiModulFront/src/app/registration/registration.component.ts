7c9ed6e89cb4fb28d752955d6d514872722742d6
DejanPredojevic <dejanpredojevic11@gmail.com>
2019-05-29 13:27:48 +0200

    Odradjen dizaj agentskog fronta

--- AgentskiModulFront/src/app/registration/registration.component.ts
+++ AgentskiModulFront/src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  SingIn:FormGroup;
+  submitted = false;
+  user: any;
+  public isLoged:boolean=false;
+  us:any;
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private router:Router,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+      this.SingIn = this.formBuilder.group({
+        name:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        surname:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        address:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        email:['',Validators.compose([Validators.required, Validators.email])],
+        username:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        password:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        confirmPassword:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])]
+      });
+  }
+
+  get f() { return this.SingIn.controls; }
+
+  onSubmit(event:any) {
+    
   }
 
 }
