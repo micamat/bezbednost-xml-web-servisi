52375a7ea389fd240e2be62f442ed3e42cbf999b
DejanPredojevic <dejanpredojevic11@gmail.com>
2019-05-30 03:15:09 +0200

    zapocet rad na funkcionalnostima

--- AgentskiModulFront/src/app/app-routing.module.ts
+++ AgentskiModulFront/src/app/app-routing.module.ts
@@ -3,11 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+import { RoomComponent } from './room/room.component';
+import { OcupationComponent } from './ocupation/ocupation.component';
+import { PricelistComponent } from './pricelist/pricelist.component';
 
 const routes: Routes = [
   {path : '', component: HomeComponent},
   {path : 'login', component : LoginComponent},
   {path : 'register', component : RegistrationComponent},
+  {path : 'accommodation', component : AccommodationComponent},
+  {path : 'room', component : RoomComponent},
+  {path : 'ocupation', component : OcupationComponent},
+  {path : 'pricelist', component : PricelistComponent},
+  
   
 ];
 
