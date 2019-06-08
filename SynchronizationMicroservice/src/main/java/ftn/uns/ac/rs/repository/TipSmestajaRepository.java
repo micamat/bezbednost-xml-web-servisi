e037cc778bc693bc498d653ab2c8920d8333325f
DejanPredojevic <dejanpredojevic11@gmail.com>
2019-06-09 00:14:08 +0200

    ima greska

---
+++ SynchronizationMicroservice/src/main/java/ftn/uns/ac/rs/repository/TipSmestajaRepository.java
@@ -0,0 +1,9 @@
+package ftn.uns.ac.rs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ftn.uns.ac.rs.model.TipSmestaja;
+
+public interface TipSmestajaRepository extends JpaRepository<TipSmestaja, Long>{
+
+}
