bc7ffa246d131d59a1e3c4b01319c545e4804ebb
jovan996 <grgurjovan.jg@gmail.com>
2019-06-21 14:48:33 +0200

    navbar

--- KorisnickiModulFront/src/app/app-routing.module.ts
+++ KorisnickiModulFront/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component: HomeComponent},
+  {path : 'login', component : LoginComponent},
+  {path : 'register', component : RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ KorisnickiModulFront/src/app/registration/registration.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  SingIn:FormGroup;
+  submitted = false;
+  user: any;
+  public isLoged:boolean=false;
+  us:any;
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private router:Router,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+      this.SingIn = this.formBuilder.group({
+        name:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        surname:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        address:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        email:['',Validators.compose([Validators.required, Validators.email])],
+        username:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        password:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])],
+        confirmPassword:['',Validators.compose([Validators.required, Validators.pattern('[a-zA-Z 0-9!]+')])]
+      });
+  }
+
+  get f() { return this.SingIn.controls; }
+
+  onSubmit(event:any) {
+    
+  }
+}
