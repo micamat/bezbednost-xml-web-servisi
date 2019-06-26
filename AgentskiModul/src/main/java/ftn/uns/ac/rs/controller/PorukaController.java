6b732fc944b2db9714eab0923db093343c92bf67
JelenaMatkovic <matkovic.jelena@uns.ac.rs>
2019-06-26 13:14:03 +0200

    sitne izmene

--- AgentskiModul/src/main/java/ftn/uns/ac/rs/controller/PorukaController.java
+++ AgentskiModul/src/main/java/ftn/uns/ac/rs/controller/PorukaController.java
@@ -22,7 +22,7 @@ public class PorukaController {
 	@Autowired
 	private PorukaService porukaService;
 
-	@GetMapping(value = "/{idRezervacija}")
+	@GetMapping(value = "/rezervacija/{idRezervacija}")
 	public ResponseEntity<List<ShowPorukaDTO>> getAllByRezervacija(@PathVariable Long idRezervacija){
 		if(porukaService.getAllByRezervacija(idRezervacija) == null) {
 			return new ResponseEntity<List<ShowPorukaDTO>>(HttpStatus.NO_CONTENT);
