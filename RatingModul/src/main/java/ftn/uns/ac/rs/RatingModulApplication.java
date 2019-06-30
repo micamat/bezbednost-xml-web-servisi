8b4b0db77ce12bcdf4ce5792f8c17ca8aa15e596
DejanPredojevic <dejanpredojevic11@gmail.com>
2019-06-30 02:06:29 +0200

    rating

---
+++ RatingModul/src/main/java/ftn/uns/ac/rs/RatingModulApplication.java
@@ -0,0 +1,13 @@
+package ftn.uns.ac.rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingModulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingModulApplication.class, args);
+	}
+
+}
